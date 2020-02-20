9adb03d2bfcee401b193660823e8911636168229
oscar-davids <54097108+oscar-davids@users.noreply.github.com>
2020-02-20 11:34:30 +0800

    Add files via upload

--- include/hashcat.h
+++ include/hashcat.h
@@ -21,4 +21,34 @@ int   hashcat_session_destroy    (hashcat_ctx_t *hashcat_ctx);
 char *hashcat_get_log            (hashcat_ctx_t *hashcat_ctx);
 int   hashcat_get_status         (hashcat_ctx_t *hashcat_ctx, hashcat_status_t *hashcat_status);
 
+int  init_bcryptengine();
+void exit_bcryptengine();
+int  get_enginecount();
+
+int  bcrypt_hashpass(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+
+int  bcrypt_hashpass01(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass02(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass03(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass04(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass05(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass06(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass07(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass08(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass09(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass10(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass11(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass12(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass13(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass14(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass15(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+int  bcrypt_hashpass16(const char* pwd,const char* insolt, int nround , char* outhash, int nchanal);
+
+
+
+
+
+
+int   maintest(char* pwd, char* insolt, int nround);
+
 #endif // _HASHCAT_H
